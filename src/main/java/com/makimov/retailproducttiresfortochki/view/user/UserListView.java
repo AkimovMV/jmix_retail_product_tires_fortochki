597f113f19e94d79e7093d57318abf0be61670bf
Maxim <m.akimov@shininvest.ru>
2026-02-26 10:27:51 +0500

    first

---
+++ src/main/java/com/makimov/retailproducttiresfortochki/view/user/UserListView.java
@@ -0,0 +1,14 @@
+package com.makimov.retailproducttiresfortochki.view.user;
+
+import com.makimov.retailproducttiresfortochki.entity.User;
+import com.makimov.retailproducttiresfortochki.view.main.MainView;
+import com.vaadin.flow.router.Route;
+import io.jmix.flowui.view.*;
+
+@Route(value = "users", layout = MainView.class)
+@ViewController(id = "User.list")
+@ViewDescriptor(path = "user-list-view.xml")
+@LookupComponent("usersDataGrid")
+@DialogMode(width = "64em")
+public class UserListView extends StandardListView<User> {
+}
